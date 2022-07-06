11bfbc1b0a874b139c5114e2a8a6b2cde2a41466
Olondar <olondar@yandex.ru>
2022-07-06 19:55:10 +0300

    Создали проект if_else

---
+++ IF_ELSE/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше пользователя");
+ string username = Console.ReadLine();
+ if (username.ToLower() == "маша") {
+    Console.WriteLine("Привет, Маша");
+ } else {
+    Console.WriteLine("Я тебя не знаю");
+ }
+ 
